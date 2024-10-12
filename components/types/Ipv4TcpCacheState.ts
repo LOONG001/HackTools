cdb7d449fc041d997668f6eeb50ed6cbe227d380
ev1l0o4g <34571314+LOONG001@users.noreply.github.com>
2024-10-12 11:26:24 +0800

    1

--- components/types/Ipv4TcpCacheState.ts
+++
@@ -1,7 +0,0 @@
-export type Ipv4TcpCacheState = {
-    ip: string | number;
-    port: string | number;
-    output_file_name?: string | number;
-    target_file_name?: string | number;
-    shell?: string | number;
-};
